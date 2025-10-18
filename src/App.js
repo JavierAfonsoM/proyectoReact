e68f8255e34faa21186a6325feeaf38fc82ef6c0
Javier <DQO93313@educastur.es>
2025-10-18 19:18:15 +0200

    Primer commit del proyecto React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <h2>Este es mi primer componente React</h2>
+      <div className="parrafo">
+        <p>Bienvenido a mi aplicacion,esto fue creado con JavaScript en React</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
